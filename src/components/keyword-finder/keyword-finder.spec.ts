560f265c1b3c2ee3126b5ec6f91c2a09738ffaad
bleuf1sh <2596120+bleuf1sh@users.noreply.github.com>
2019-03-04 12:10:05 -0600

    added more tests to keywordfinder

--- src/components/keyword-finder/keyword-finder.spec.ts
+++ src/components/keyword-finder/keyword-finder.spec.ts
@@ -5,9 +5,25 @@ import { KeywordFinder } from "./keyword-finder";
 describe("KeywordFinder", () => {
   
   it("should save and retrieve regex keyword list", () => {
-    const regexKeywordList = []
-    const keywordFinder = new KeywordFinder([]);
-    expect(keywordFinder.getRegexKeywordList()).toEqual([]);
+    const keywordFinderEmpty = new KeywordFinder([]);
+    expect(keywordFinderEmpty.getRegexKeywordList()).toEqual([]);
+
+    const randomString: string = ''+Date.now();
+    const keywordFinderRandom = new KeywordFinder([randomString]);
+    expect(keywordFinderRandom.getRegexKeywordList()).toEqual([randomString]);
+  });
+
+  it("should detect keywords fixed to word boundries", () => {    
+    const keywordFinder = new KeywordFinder(['gal']);
+    expect(keywordFinder.isKeywordPresent(null)).toBeFalsy();
+    expect(keywordFinder.isKeywordPresent('')).toBeFalsy();
+    expect(keywordFinder.isKeywordPresent('gallon')).toBeFalsy();
+
+    expect(keywordFinder.isKeywordPresent('gal')).toBeTruthy();
+    expect(keywordFinder.isKeywordPresent(' gal')).toBeTruthy();
+    expect(keywordFinder.isKeywordPresent('gal ')).toBeTruthy();
+    expect(keywordFinder.isKeywordPresent(' gal ')).toBeTruthy();
+    expect(keywordFinder.isKeywordPresent(' why gal?')).toBeTruthy();
   });
 
 });
